e6fd5495d14bc67b7c857b23ac22f973d36eb067
wendyLudibethCanales <ludibethcanales02@gmail.com>
2022-07-11 14:31:54 -0600

    Creación de la pantalla Administrador

--- tyr-shop/src/app/app.module.ts
+++ tyr-shop/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
+import { AdministradorComponent } from './components/administrador/administrador/administrador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    LoginComponent
+    LoginComponent,
+    AdministradorComponent
   ],
   imports: [
     BrowserModule,
